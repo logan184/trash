5dbe2036dcdd098982cf41c5691fa960455deb39
logan184 <76756222+logan184@users.noreply.github.com>
2021-03-10 18:30:16 -0600

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,21 @@
+class paper  {
+    constructor(x,y){  
+    var options = {
+    isStatic:false,
+    restitution:0.3,
+    friction:0.5,
+    density:1.2
+}
+
+this.image=loadImage("paper.png")
+this.body = Bodies.circle(x, y, 35,options);
+World.add(world, this.body);
+}
+display() {
+    imageMode(CENTER);
+    image(this.image,this.body.position.x,this.body.position.y,70,70);
+}
+
+
+}
+
